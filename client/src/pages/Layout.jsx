749c8709a755342ae9b2ef18df8934b1ed1d90ee
burak behlül <burakyabgu@gmail.com>
2025-04-30 02:18:06 +0300

    feat: Style Layout component

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,10 +1,17 @@
+import { Container } from "@chakra-ui/react"
 import Routes from "../routes/index"
+
 
 function Layout() {
   return (
-	<>
+	<Container
+		padding="15px"
+		bg={"gray.100"}
+		minH="100vh"
+		maxW="100vw"
+	>
 		<Routes />
-	</>
+	</Container>
   )
 }
 
