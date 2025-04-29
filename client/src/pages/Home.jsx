5f92b2d555413953263b2bb347a6751cb3164fc0
burak behlül <burakyabgu@gmail.com>
2025-04-30 02:52:54 +0300

    feat: Style Home Component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,25 @@
-
+import { Flex, Box } from "@chakra-ui/react"
 function Home() {
   return (
 	<>
-		HOME COMPONENT
+		<Flex
+			direction={{ base: "column", md: "row" }} // Mobilde dikey, md ve yukarısında yatay
+			height="100vh"
+			>
+			<Box
+				flex={{ base: 1, md: 3 }} // Mobilde eşit oran (1), md ve yukarısında %30
+				bg="blue.200"
+			>
+				Sol Alan
+			</Box>
+
+			<Box
+				flex={{ base: 1, md: 7 }} // Mobilde eşit oran (1), md ve yukarısında %70
+				bg="green.200"
+			>
+				Sağ Alan
+			</Box>
+		</Flex>
 	</>
   )
 }
